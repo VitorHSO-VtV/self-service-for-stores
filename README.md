14db575ff5aa2ddcaa7e0f680c9d749960096502
VitorHSO-VtV <vitor.oficial.9008@gmail.com>
2024-01-02 22:59:30 -0300

    criei uma tela nova

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # self-service-for-stores
-self-service system for stores, the customer can place their own order without the need for a cashier, this system aims to replace the cashier with a machine, reducing employee costs
+The customer can place their own order without the need for a cashier, this system aims to replace the cashier with a machine, reducing employee costs
+
+# counted days
+I started counting the days around the 30th because I only had this idea at that moment, but I'm going to start counting as if it were the 1st day
+
+# Day 1 - 02/01/2024
+
+My goal for today is to finish the cart system so that the customer can enter this tab and complete the purchase
+
+Today I started by transferring some variables and folders to English since I originally wrote several parts in my native language, Portuguese
--- machine/main.py
+++ machine/main.py
@@ -8,7 +8,6 @@ from kivy.app import App
 import functions as fc
 import os
 
-
 tot = 0
 
 
@@ -18,8 +17,8 @@ class Manager(ScreenManager):
 
 class InitScreen(Screen):
     def on_enter(self, *args):
-        fc.clear_file('manager_files/carrinho.txt')
-        fc.clear_file('manager_files/nota_fiscal.txt')
+        fc.clear_file('manager_files/cart.txt')
+        fc.clear_file('manager_files/invoice.txt')
         global tot
         tot = 0
 
@@ -38,12 +37,11 @@ class HomeBar(BoxLayout):
         app.root.transition.direction = 'down'
         app.root.current = 'InitScreen'
 
+
 class BuyScreen(Screen):
     def on_pre_leave(self):
         fade_out = Animation(opacity=0, duration=0.5)
-        fade_out.start(self.ids.Class_Buy1)
-        fade_out.start(self.ids.Class_Buy2)
-        fade_out.start(self.ids.CarBar)
+        fade_out.start(self.ids.BuyScreen)
 
     def on_pre_enter(self, *args):
         super().on_enter(*args)
@@ -55,22 +53,27 @@ class BuyScreen(Screen):
                 SubClassCreate(text1='bebidas', sub_classe1=sub_classes[sub_classe], text2='bebidas',
                                sub_classe2=sub_classes[sub_classe + 1]))
         if len(sub_classes) % 2 != 0:
-            self.ids.Class_Buy2.add_widget(SCCimpar(text1='bebidas', sub_classe1=sub_classes[len(sub_classes) - 1]))
+            self.ids.Class_Buy2.add_widget(SCCodd(text1='bebidas', sub_classe1=sub_classes[len(sub_classes) - 1]))
 
         anim = Animation(opacity=1, duration=0.5)
-        anim.start(self.ids.Class_Buy1)
-        anim.start(self.ids.Class_Buy2)
-        anim.start(self.ids.CarBar)
+        anim.start(self.ids.BuyScreen)
 
         classes = os.listdir('templates/classes_normal')
         for classe in classes:
             self.ids.Class_Buy1.add_widget(
                 ClassCreate(text=classe.replace('.png', ''), image_source=f'templates/classes_normal/{classe}'))
 
-        self.ids.CarBar.clear_widgets()
-        itens = fc.txt_to_py('manager_files/carrinho.txt')
-        for item in range(0, len(itens), 2):
-            self.ids.CarBar.add_widget(CarConstruct(itens[item], float(itens[item + 1])))
+        self.ids.CartBar.clear_widgets()
+        items = fc.txt_to_py('manager_files/cart.txt')
+        for item in range(0, len(items), 2):
+            self.ids.CartBar.add_widget(CartConstruct(items[item], float(items[item + 1])))
+
+    @staticmethod
+    def on_cart_button_release():
+        app = App.get_running_app()
+        app.root.transition.duration = 1.0
+        app.root.transition.direction = 'up'
+        app.root.current = 'CarScreen'
 
     def trigger_buy_screen(self, btn1_text):
         self.ids.Class_Buy2.clear_widgets()
@@ -80,24 +83,23 @@ class BuyScreen(Screen):
                 SubClassCreate(text1=btn1_text, sub_classe1=sub_classes[sub_classe], text2=btn1_text,
                                sub_classe2=sub_classes[sub_classe + 1]))
         if len(sub_classes) % 2 != 0:
-            self.ids.Class_Buy2.add_widget(SCCimpar(text1=btn1_text, sub_classe1=sub_classes[len(sub_classes) - 1]))
+            self.ids.Class_Buy2.add_widget(SCCodd(text1=btn1_text, sub_classe1=sub_classes[len(sub_classes) - 1]))
 
 
-class CarConstruct(BoxLayout):
+class CartConstruct(BoxLayout):
     def __init__(self, text, price, **kwargs):
         super().__init__(**kwargs)
         self.ids.item.text = text
         self.text = text
         self.price = price
-        fc.file_append('manager_files/nota_fiscal.txt', self.text)
 
     def remove_self(self):
         parent_layout = self.parent
         if parent_layout:
             parent_layout.remove_widget(self)
-            fc.remove_line_from_file('manager_files/carrinho.txt', self.text + '\n')
-            fc.remove_line_from_file('manager_files/carrinho.txt', str(self.price) + '\n')
-            fc.remove_line_from_file('manager_files/nota_fiscal.txt', self.text)
+            fc.remove_line_from_file('manager_files/cart.txt', self.text + '\n')
+            fc.remove_line_from_file('manager_files/cart.txt', str(self.price) + '\n')
+            fc.remove_line_from_file('manager_files/invoice.txt', self.text + '\n')
             global tot
             tot -= self.price
             print(tot)
@@ -105,9 +107,10 @@ class CarConstruct(BoxLayout):
     def duplicate_self(self):
         parent_layout = self.parent
         if parent_layout:
-            parent_layout.add_widget(CarConstruct(text=self.text, price=self.price))
-            fc.file_append('manager_files/carrinho.txt', self.text)
-            fc.file_append('manager_files/carrinho.txt', self.price)
+            parent_layout.add_widget(CartConstruct(text=self.text, price=self.price))
+            fc.file_append('manager_files/cart.txt', self.text)
+            fc.file_append('manager_files/cart.txt', self.price)
+            fc.file_append('manager_files/invoice.txt', self.text)
             global tot
             tot += self.price
             print(tot)
@@ -156,7 +159,7 @@ def on_button_release(text, sub_classe):
     app.root.current = 'ItemScreen'
 
 
-class SCCimpar(BoxLayout):
+class SCCodd(BoxLayout):
     def __init__(self, text1='', sub_classe1='', **kwargs):
         super().__init__(**kwargs)
 
@@ -170,6 +173,8 @@ class SCCimpar(BoxLayout):
 class ItemScreen(Screen):
     def __init__(self, **kwargs):
         super(ItemScreen, self).__init__(**kwargs)
+        self.price = None
+        self.name_class = None
         self.ids['food'] = Image()
 
         with self.canvas:
@@ -209,14 +214,16 @@ class ItemScreen(Screen):
         try:
             global tot
             tot += float(self.price)
-            fc.file_append('manager_files/carrinho.txt', f'{self.name_class}R${self.price.replace('.', ',')}')
-            fc.file_append('manager_files/carrinho.txt', float(self.price))
+            fc.file_append('manager_files/cart.txt', f'{self.name_class}R${self.price.replace('.', ',')}')
+            fc.file_append('manager_files/cart.txt', float(self.price))
+            fc.file_append('manager_files/invoice.txt', f'{self.name_class}R${self.price.replace('.', ',')}')
         except AttributeError:
             print('object has no attribute price')
         except Exception as e:
             print(f'Error: {e}')
 
-    def on_back_button_release(self):
+    @staticmethod
+    def on_back_button_release():
         app = App.get_running_app()
         app.root.transition.duration = 1.0
         app.root.transition.direction = 'down'
@@ -242,6 +249,31 @@ class ListCreate(BoxLayout):
     def __init__(self, text='', price='', **kwargs):
         super().__init__(**kwargs)
         self.ids.item1.text = f'[b][color=000000]{text}[/color] [color=FA2B2B]R${price}[/color][/b]'
+
+class CarScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        pass
+
+    def on_pre_enter(self, *args):
+        anim = Animation(opacity=1, duration=0.5)
+        anim.start(self.ids.CarScreen)
+
+        self.ids.invoice.clear_widgets()
+        items = fc.txt_to_py('manager_files/cart.txt')
+        for item in range(0, len(items), 2):
+            self.ids.invoice.add_widget(CartConstruct(items[item], float(items[item + 1])))
+
+    def on_pre_leave(self, *args):
+        anim = Animation(opacity=0, duration=0.5)
+        anim.start(self.ids.CarScreen)
+
+    @staticmethod
+    def on_back_button_release():
+        app = App.get_running_app()
+        app.root.transition.duration = 1.0
+        app.root.transition.direction = 'down'
+        app.root.current = 'BuyScreen'
 
 
 class Shopping(App):
